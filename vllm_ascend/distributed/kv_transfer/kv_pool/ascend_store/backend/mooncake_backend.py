77525b9155f383cfb6b4b452e88655d8a6825059
Chao Lei <leichao139636@163.com>
2026-06-03 21:26:52 +0800

    [Feature][v0.20.2rc] Mooncake kvpool usage optimization (#7820) (#9947)
    ### What this PR does / why we need it?
    
      Backport of #7820 to `releases/v0.20.2rc`.
    
      Mooncake kvpool usage optimization, including:
    
    1. Expose the `preferred_segment` and `prefer_alloc_in_same_node`
    Mooncake
         parameters so users can configure them as needed.
      2. Convert `setup` to keyword-argument calls to accommodate different
         versions of Mooncake.
    3. Make `master_server_address` retrievable from both environment
    variables
    and configuration files, with environment variables taking precedence.
    
    Signed-off-by: LCAIZJ <leichao139636@163.com>

--- vllm_ascend/distributed/kv_transfer/kv_pool/ascend_store/backend/mooncake_backend.py
+++ vllm_ascend/distributed/kv_transfer/kv_pool/ascend_store/backend/mooncake_backend.py
@@ -107,25 +107,25 @@ class MooncakeBackend(Backend):
             transfer_engine = global_te.get_transfer_engine(local_hostname, device_name=None)
             self.local_seg = local_hostname + ":" + str(transfer_engine.get_rpc_port())
             ret = store.setup(
-                self.local_seg,
-                self.config.metadata_server,
-                self.config.global_segment_size,
-                self.config.local_buffer_size,
-                self.config.protocol,
-                self.config.device_name,
-                self.config.master_server_address,
-                transfer_engine.get_engine(),
+                local_hostname=self.local_seg,
+                metadata_server=self.config.metadata_server,
+                global_segment_size=self.config.global_segment_size,
+                local_buffer_size=self.config.local_buffer_size,
+                protocol=self.config.protocol,
+                rdma_devices=self.config.device_name,
+                master_server_addr=self.config.master_server_address,
+                engine=transfer_engine.get_engine(),
             )
         else:
             self.local_seg = local_hostname
             ret = store.setup(
-                self.local_seg,
-                self.config.metadata_server,
-                self.config.global_segment_size,
-                0,
-                self.config.protocol,
-                self.config.device_name,
-                self.config.master_server_address,
+                local_hostname=self.local_seg,
+                metadata_server=self.config.metadata_server,
+                global_segment_size=self.config.global_segment_size,
+                local_buffer_size=0,
+                protocol=self.config.protocol,
+                rdma_devices=self.config.device_name,
+                master_server_addr=self.config.master_server_address,
             )
 
         if ret != 0:
@@ -161,7 +161,11 @@ class MooncakeBackend(Backend):
         try:
             self._ensure_initialized()
             assert self.store is not None
-            res = self.store.batch_put_from_multi_buffers(keys, addrs, sizes)
+            config = ReplicateConfig()
+            if self.config.preferred_segment:
+                config.preferred_segment = self.local_seg
+            config.prefer_alloc_in_same_node = self.config.prefer_alloc_in_same_node
+            res = self.store.batch_put_from_multi_buffers(keys, addrs, sizes, config)
             for value in res:
                 if value < 0:
                     logger.error("Failed to put key %s,res:%s", keys, res)
