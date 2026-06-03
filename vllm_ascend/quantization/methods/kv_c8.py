11b0e8dafd0dcc872fc19936501968b2999c6e3a
Yaphets24 <44045681+Yaphets24@users.noreply.github.com>
2026-06-03 15:22:05 +0800

    [Misc] Improve logging in quantization (#9880)
    ### What this PR does / why we need it?
    Back port from #9621. Improve logging coverage, error context, and log level correctness in
    quantization.
    
    ### Does this PR introduce _any_ user-facing change?
    No.
    
    ### How was this patch tested?
    NA.
    
    Signed-off-by: mayumeng <m30059191@china.huawei.com>
    Co-authored-by: mayumeng <m30059191@china.huawei.com>

--- vllm_ascend/quantization/methods/kv_c8.py
+++ vllm_ascend/quantization/methods/kv_c8.py
@@ -5,8 +5,6 @@ from vllm.logger import logger
 
 from vllm_ascend.utils import AscendDeviceType, get_ascend_device_type
 
-from vllm_ascend.utils import AscendDeviceType, get_ascend_device_type
-
 from .base import AscendAttentionScheme
 from .registry import register_scheme
 
