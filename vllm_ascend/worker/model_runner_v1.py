6e9e8992c201916f8523b311f55862cba6c8dda0
aolin123 <zhangaolin5@huawei.com>
2026-06-10 10:29:58 +0800

    fix kv_tensor.numel

--- vllm_ascend/worker/model_runner_v1.py
+++ vllm_ascend/worker/model_runner_v1.py
@@ -7038,17 +7038,17 @@ class NPUModelRunner(GPUModelRunner):
                     for layer_name_inner in kv_cache_tensor.shared_by:
                         # shared the kvcache for all shared layers
                         kv_cache_raw_tensors[layer_name_inner] = tensor
-                # elif "attn" in layer_name and self.use_compress and layer_name not in kv_cache_raw_tensors.keys(
-                # ):
-                #     if self.vllm_config.kv_transfer_config is None:
-                #         tensor = torch.zeros(kv_cache_tensor.size, dtype=torch.int8, device=self.device)
-                #     else:
-                #         cache_size_aligned = kv_cache_tensor.size + alignment
-                #         tensor = torch.zeros(cache_size_aligned, dtype=torch.int8, device=self.device)
-                #         tensor = self._align_memory(tensor, alignment)[: kv_cache_tensor.size]
-                #     for layer_name_inner in kv_cache_tensor.shared_by:
-                #         # shared the kvcache between the self_attn specs in the same group
-                #         kv_cache_raw_tensors[layer_name_inner] = tensor
+                elif "attn" in layer_name and self.use_compress and layer_name not in kv_cache_raw_tensors.keys(
+                ):
+                    if self.vllm_config.kv_transfer_config is None:
+                        tensor = torch.zeros(kv_cache_tensor.size, dtype=torch.int8, device=self.device)
+                    else:
+                        cache_size_aligned = kv_cache_tensor.size + alignment
+                        tensor = torch.zeros(cache_size_aligned, dtype=torch.int8, device=self.device)
+                        tensor = self._align_memory(tensor, alignment)[: kv_cache_tensor.size]
+                    for layer_name_inner in kv_cache_tensor.shared_by:
+                        # shared the kvcache between the self_attn specs in the same group
+                        kv_cache_raw_tensors[layer_name_inner] = tensor
                 elif "attn" in layer_name and layer_name not in kv_cache_raw_tensors and not use_mamba:
                     # NOTE: We need to init k cache tensor (nope cache tensor in mla) and
                     # v cache tensor (rope cache tensor in mla) separately to support prefill disaggregation,
