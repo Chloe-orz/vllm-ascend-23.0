649d052273a867a6189d59a339187ad5f3b02fd0
胡琪琪 <huqiqi@MacBook-Pro.local>
2026-06-17 21:03:49 +0800

    minimax update cos sin

--- vllm_ascend/patch/worker/patch_minimax_m2.py
+++ vllm_ascend/patch/worker/patch_minimax_m2.py
@@ -35,7 +35,7 @@ from vllm.model_executor.models.minimax_m2 import (
 from vllm.platforms import current_platform
 from vllm.sequence import IntermediateTensors
 
-from vllm_ascend.ops.rotary_embedding import get_cos_and_sin_slice
+from vllm_ascend.ops.rotary_embedding import get_cos_and_sin_slice, update_cos_sin
 from vllm_ascend.utils import enable_sp
 
 FP8_DTYPES = tuple(
@@ -101,6 +101,7 @@ def _patch_forward(
     hidden_states: torch.Tensor,
 ) -> torch.Tensor:
     qkv, _ = self.qkv_proj(hidden_states)
+    update_cos_sin(positions)
     cos, sin = get_cos_and_sin_slice()
     q, k, v = torch.ops.vllm.split_qkv_tp_rmsnorm_rope(
         input=qkv,
