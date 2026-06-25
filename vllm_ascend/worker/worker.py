fa92bd4a821c17e4c73105c5183c9c1ba5856f80
aolin123 <zhangaolin5@huawei.com>
2026-06-25 10:57:01 +0800

    rm e2c residual

--- vllm_ascend/worker/worker.py
+++ vllm_ascend/worker/worker.py
@@ -396,6 +396,7 @@ class NPUWorker(WorkerBase):
                 hidden_dtype=hidden_dtype,
                 has_residual=has_residual,
                 hc_mult=hc_mult,
+                mode=self.model_runner.edge_cloud_cfg.mode,
             )
 
     @torch.inference_mode()
